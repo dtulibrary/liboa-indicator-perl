afff8c77a2a2ea53e053faae8fa009f6aee9ddd9
franck <franck@cvt.dk>
2015-07-03 10:23:34 +0200

    Added proper logging through the OA::Indicator object and added a number of fields including research area, BFI jno, ISSN and authors

--- lib/OA/Indicator/DB/BFI.pm
+++ lib/OA/Indicator/DB/BFI.pm
@@ -7,10 +7,11 @@ our $VERSION = '1.0';
 
 sub new
 {
-    my ($class, $db) = @_;
+    my ($class, $db, $oai) = @_;
     my $self = {};
 
     $self->{'db'} = $db;
+    $self->{'oai'} = $oai;
     return (bless ($self, $class));
 }
 
@@ -23,15 +24,20 @@ sub create
                    id              text primary key,
                    publication_id  text,
                    source          text,
-                   doc_type        text,                                                                                                                
-                   doc_review      text,                                                                                                                
-                   doc_level       text,                                                                                                                
-                   type            text,                                                                                                                
-                   fraction        text,                                                                                                                
-                   point           text,                                                                                                                
-                   cooperation     text,                                                                                                                
+                   doc_type        text,
+                   doc_review      text,
+                   doc_level       text,
+                   class           text,
+                   research_area   text,
+                   journal_level   integer,
+                   fraction        text,
+                   point           text,
+                   cooperation     text,
                    title           text,
                    lang            text,
+                   issn            text,
+                   eissn           text,
+                   authors         text,
                    mods            text,
                    original_xml    text
               )');
@@ -44,8 +50,11 @@ sub load
     my ($fin);
 
     if (!open ($fin, "zcat /var/lib/oa-indicator/$year/bfi/bfi.ids.gz |")) {
-        die ("fatal: failed to open /var/lib/oa-indicator/$year/bfi/bfi.ids.gz ($!)");
+        $self->{'oai'}->log ('f',  "failed to open /var/lib/oa-indicator/$year/bfi/bfi.ids.gz ($!)");
+        $self->{'oai'}->log ('f',  'failed');
+        return (0);
     }
+    my $count = 0;
     while (<$fin>) {
         chomp;
         if (m/^#/) {
@@ -53,14 +62,25 @@ sub load
         }
         my $rec = {};
         my @fields = split ("\t");
-        foreach my $fld (qw(id pubid source doc_type doc_review doc_level type fraction point cooperation title lang)) {
+        foreach my $fld (qw(id publication_id source doc_type doc_review doc_level class research_area journal_level fraction point cooperation title lang issn eissn authors)) {
             $rec->{$fld} = shift (@fields);
         }
         $rec->{'source'} = lc ($rec->{'source'});
+        if (!$rec->{'journal_level'}) {
+            $rec->{'journal_level'} = 0;
+        } else {
+            if (($rec->{'journal_level'} != 1) && ($rec->{'journal_level'} != 2)) {
+                $rec->{'journal_level'} = 0;
+            }
+        }
         $rec->{'mods'} = $rec->{'original_xml'} = '';
         $self->{'db'}->insert ('bfi', $rec);
+        $count++;
     }
     close ($fin);
+    $self->{'oai'}->log ('i',  "loaded $count records");
+    $self->{'oai'}->log ('i',  'done');
+    return (1);
 }
 
 sub pubid
