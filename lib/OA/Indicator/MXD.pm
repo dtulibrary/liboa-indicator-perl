3a2b39432ac2c41db332b0911fe2b743fc10bfc3
franck <franck@cvt.dk>
2015-07-03 10:27:13 +0200

    Added a lot of new fields, including persons fields which are now extracted as well.

--- lib/OA/Indicator/MXD.pm
+++ lib/OA/Indicator/MXD.pm
@@ -9,25 +9,40 @@ sub new
     my ($class) = @_;
     my $self = {};
 
-    $self->{'primary_fields'} = [qw(source_id year type level review)];
+    $self->{'primary_fields'} = [qw(source_id year type level review research_area jno pno jtitle jtitle_alt)];
     $self->{'xpath'} = {
-        source_id  => '/mxd:ddf_doc/@rec_id',
-        year       => '/mxd:ddf_doc/@doc_year',
-        type       => '/mxd:ddf_doc/@doc_type',
-        level      => '/mxd:ddf_doc/@doc_level',
-        review     => '/mxd:ddf_doc/@doc_review',
-        do         => '/mxd:ddf_doc/mxd:publication/mxd:digital_object',
-        do_role    => '@role',
-        do_access  => '@access',
-        do_size    => 'mxd:file/@size',
-        do_mime    => 'mxd:file/@mime_type',
-        do_file    => 'mxd:file/@filename',
-        do_uri     => 'mxd:uri',
-        inet       => '/mxd:ddf_doc/mxd:publication/mxd:inetpub',
-        inet_text  => 'mxd:text',
-        inet_uri   => 'mxd:uri',
-        pissn      => '//mxd:issn[@type="pri"]',
-        eissn      => '//mxd:issn[@type="ele"]',
+        source_id            => '/mxd:ddf_doc/@rec_id',
+        year                 => '/mxd:ddf_doc/@doc_year',
+        type                 => '/mxd:ddf_doc/@doc_type',
+        level                => '/mxd:ddf_doc/@doc_level',
+        review               => '/mxd:ddf_doc/@doc_review',
+        research_area        => '/mxd:ddf_doc/mxd:description/mxd:research_area/@area_code',
+        jno                  => '/mxd:ddf_doc/mxd:publication/@bfi_serial_no',
+        pno                  => '/mxd:ddf_doc/mxd:publication/@bfi_publisher_no',
+        jtitle               => '/mxd:ddf_doc/mxd:publication/mxd:in_journal/mxd:title',
+        jtitle_alt           => '/mxd:ddf_doc/mxd:publication/mxd:in_journal/mxd:title_alternative',
+        do                   => '/mxd:ddf_doc/mxd:publication/mxd:digital_object',
+        do_role              => '@role',
+        do_access            => '@access',
+        do_size              => 'mxd:file/@size',
+        do_mime              => 'mxd:file/@mime_type',
+        do_file              => 'mxd:file/@filename',
+        do_uri               => 'mxd:uri',
+        inet                 => '/mxd:ddf_doc/mxd:publication/mxd:inetpub',
+        inet_text            => 'mxd:text',
+        inet_uri             => 'mxd:uri',
+        inet_access          => 'mxd:uri/@access',
+        pissn                => '//mxd:issn[@type="pri"]',
+        eissn                => '//mxd:issn[@type="ele"]',
+        person               => '/mxd:ddf_doc/mxd:person',
+        person_role          => '@pers_role',
+        person_first         => 'mxd:name/mxd:first',
+        person_last          => 'mxd:name/mxd:last',
+        person_id            => 'mxd:id',
+        person_id_type       => '@id_type',
+        person_id_source     => '@id_source',
+        person_id_id         => '.',
+        person_email         => 'mxd:email',
     };
     $self->{'xml'} = new OA::Indicator::XML (mxd => 'http://mx.forskningsdatabasen.dk/ns/documents/1.3');
     return (bless ($self, $class));
@@ -73,7 +88,7 @@ sub primary
 
 sub fulltext_fields
 {
-    return (qw(type uri text role access size mime filename));
+    return (qw(type uri access text role size mime filename));
 }
 
 sub fulltext
@@ -83,15 +98,29 @@ sub fulltext
 
     foreach my $obj ($self->{'xml'}->node ($self->{'xpath'}{'do'})) {
         my $rec = ['digital_object'];
-        foreach my $f (qw(do_uri inet_text do_role do_access do_size do_mime do_file)) {
-            push (@{$rec}, $self->{'xml'}->field ($self->{'xpath'}{$f}, $obj));
+        foreach my $f (qw(do_uri do_access blank do_role do_size do_mime do_file)) {
+            if ($f eq 'blank') {
+                push (@{$rec}, '');
+            } else {
+                my $s;
+                if ($s = $self->{'xml'}->field ($self->{'xpath'}{$f}, $obj)) {
+                    push (@{$rec}, $s);
+                } else {
+                    push (@{$rec}, '');
+                }
+            }
         }
         push (@ret, $rec);
     }
     foreach my $obj ($self->{'xml'}->node ($self->{'xpath'}{'inet'})) {
         my $rec = ['inetpub'];
-        foreach my $f (qw(inet_uri inet_text)) {
-            push (@{$rec}, $self->{'xml'}->field ($self->{'xpath'}{$f}, $obj));
+        foreach my $f (qw(inet_uri inet_access inet_text)) {
+            my $s;
+            if ($s = $self->{'xml'}->field ($self->{'xpath'}{$f}, $obj)) {
+                push (@{$rec}, $s);
+            } else {
+                push (@{$rec}, '');
+            }
         }
         push (@ret, $rec);
     }
@@ -113,6 +142,44 @@ sub issn
     }
     foreach my $issn ($self->{'xml'}->field ($self->{'xpath'}{'eissn'})) {
         push (@ret, ['electronic', $issn]);
+    }
+    return (@ret);
+}
+
+sub person_fields
+{
+    return (qw(role first last email id type source));
+}
+
+sub person
+{
+    my ($self) = @_;
+    my @ret = ();
+
+    foreach my $obj ($self->{'xml'}->node ($self->{'xpath'}{'person'})) {
+        my $rec = [];
+        foreach my $f (qw(person_role person_first person_last person_email person_id)) {
+            if ($f eq 'person_id') {
+                foreach my $id ($self->{'xml'}->node ($self->{'xpath'}{$f}, $obj)) {
+                    foreach my $fld (qw(person_id_id person_id_type person_id_source)) {
+                        my $s;
+                        if ($s = $self->{'xml'}->field ($self->{'xpath'}{$fld}, $id)) {
+                            push (@{$rec}, $s);
+                        } else {
+                            push (@{$rec}, '');
+                        }
+                    } 
+                }
+            } else {
+                my $s;
+                if ($s = $self->{'xml'}->field ($self->{'xpath'}{$f}, $obj)) {
+                    push (@{$rec}, $s);
+                } else {
+                    push (@{$rec}, '');
+                }
+            }
+        }
+        push (@ret, $rec);
     }
     return (@ret);
 }
