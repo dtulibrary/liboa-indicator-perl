80b1780fb2c4b0a2e2382429db3c4b32ee200740
franck <franck@cvt.dk>
2015-07-03 15:02:11 +0200

    An updated load of Romeo data, this time based on the actual data harvested from DS2

--- lib/OA/Indicator/DB/Romeo.pm
+++ lib/OA/Indicator/DB/Romeo.pm
@@ -7,10 +7,11 @@ our $VERSION = '1.0';
 
 sub new
 {
-    my ($class, $db) = @_;
+    my ($class, $db, $oai) = @_;
     my $self = {};
 
     $self->{'db'} = $db;
+    $self->{'oai'} = $oai;
     return (bless ($self, $class));
 }
 
@@ -20,14 +21,21 @@ sub create
 
     my $db = $self->{'db'};
     $db->sql ('create table if not exists romeo (
-                   id              integer primary key,
-                   issn            text,                                                                                                                
-                   publisher_id    integer,
+                   id              text primary key,
+                   harvest_stamp   integer,
+                   harvest_date    text,
+                   source_id       text,
+                   pissn           text,
+                   eissn           text,
                    color           text,
+                   publisher       text,
+                   title           text,
                    mods            text,
                    original_xml    text
               )');
-    $db->sql ('create index if not exists romeo_issn on romeo (issn)');
+    $db->sql ('create index if not exists romeo_source_id on romeo (source_id)');
+    $db->sql ('create index if not exists romeo_pissn on romeo (pissn)');
+    $db->sql ('create index if not exists romeo_eissn on romeo (eissn)');
 }
 
 sub load
@@ -36,20 +44,77 @@ sub load
     my ($fin);
 
     if (!open ($fin, "zcat /var/lib/oa-indicator/$year/romeo/romeo.ids.gz |")) {
-        die ("fatal: failed to open /var/lib/oa-indicator/$year/romeo/romeo.ids.gz ($!)");
+        $self->{'oai'}->log ('f',  "failed to open /var/lib/oa-indicator/$year/romeo/romeo.ids.gz ($!)");
+        $self->{'oai'}->log ('f',  'failed');
+        return (0);
     }
+    my $count = {};
+    my $records = {};
     while (<$fin>) {
         chomp;
         if (m/^#/) {
             next;
         }
         my $rec = {};
-        ($rec->{'issn'}, $rec->{'publisher_id'}, $rec->{'color'}) = split ("\t");
-        $rec->{'issn'} = $self->issn_normalize ($rec->{'issn'});
-        $rec->{'mods'} = $rec->{'original_xml'} = '';
-        $self->{'db'}->insert ('romeo', $rec);
+        my @fields = split ("\t");
+        for my $fld (qw(id harvest_stamp harvest_date status source_id pissn eissn color publisher title)) {
+            $rec->{$fld} = shift (@fields);
+        }
+        if ($rec->{'status'} ne 'deleted') {
+            $rec->{'source_id'} = $self->issn_normalize ($rec->{'source_id'});
+            $rec->{'pissn'} = $self->issn_normalize ($rec->{'pissn'});
+            $rec->{'eissn'} = $self->issn_normalize ($rec->{'eissn'});
+            $rec->{'mods'} = $rec->{'original_xml'} = '';
+            delete ($rec->{'status'});
+            $records->{$rec->{'id'}} = $rec;
+            $count->{'rows'}++;
+        }
     }
     close ($fin);
+    if (!open ($fin, "zcat /var/lib/oa-indicator/$year/romeo/romeo.xml.gz |")) {
+        $self->{'oai'}->log ('f', "failed to open /var/lib/oa-indicator/$year/romeo/romeo.xml.gz ($!)");
+        $self->{'oai'}->log ('f', 'failed');
+        return (0);
+    }
+    my $buf = '';
+    while (<$fin>) {
+        chomp;
+        $buf .= $_;
+        $buf =~ s/^[\s\t\r\n]*<\?xml .*?\?>//;
+        $buf =~ s/^[\s\t\r\n]*<records>//;
+        $buf =~ s/^[\s\t\r\n]+//;
+        while ($buf =~ s/^.*?(<mods .*?<\/mods>)//s) {
+            my $xml = $1;
+            if ($xml =~ m/<identifier [^>]*type="ds.dtic.dk:id:pub:dads:recordid">([^<]+)<\/identifier>/) {
+                my $id = $1;
+                if (!exists ($records->{$id})) {
+                    $self->{'oai'}->log ('f', "id does not exist: $id in romeo");
+                    $self->{'oai'}->log ('f', 'failed');
+                    return (0);
+                }
+                $records->{$id}{'mods'} = $xml;
+                $self->{'db'}->insert ('romeo', $records->{$id});
+                delete ($records->{$id});
+                $count->{'romeo'}++;
+                if (($count->{'romeo'} % 1000) == 0) {
+                    $self->{'oai'}->log ('i', "loaded $count->{'romeo'} of $count->{'rows'}");
+                }
+            } else {
+                $self->{'oai'}->log ('f', " could not get identifier from MODS XML:\n$xml");
+                $self->{'oai'}->log ('f', 'failed');
+                return (0);
+            }
+        }
+    }
+    $self->{'oai'}->log ('i', "loaded $count->{'romeo'} of $count->{'rows'}");
+    foreach my $id (keys (%{$records})) {
+        $count->{'missing'}++;
+    }
+    if ($count->{'missing'}) {
+        $self->{'oai'}->log ('w', "$count->{'missing'} missing MODS");
+    }
+    $self->{'oai'}->log ('i',  'done');
+    return (1);
 }
 
 sub issn_normalize
@@ -65,20 +130,28 @@ sub color
 {
     my ($self, $issn) = @_;
 
+    if (!exists ($self->{'cache'})) {
+        $self->{'cache'} = {};
+        my $rs = $self->{'db'}->select ('id,source_id,pissn,eissn,color', 'romeo');
+        my $rec;
+        while ($rec = $self->{'db'}->next ($rs)) {
+            foreach my $fld (qw(source_id pissn eissn)) {
+                if (exists ($self->{'cache'}{$rec->{$fld}})) {
+                    if ($self->{'cache'}{$rec->{$fld}}->[0] ne $rec->{'color'}) {
+                        $self->{'oai'}->log ('w',  "existing ISSN with two different colors: $rec->{$fld} : $self->{'cache'}{$rec->{$fld}}->[0] != $rec->{'color'}");
+                    }
+                } else {
+                    $self->{'cache'}{$rec->{$fld}} = [$rec->{'color'}, $rec->{'id'}];
+                }
+            }
+        }
+    }
     $issn = $self->issn_normalize ($issn);
     if ($self->{'cache'}{$issn}) {
-        return (@{$self->{'cache'}{$issn}});
-    }
-    my $rs = $self->{'db'}->select ('id,color', 'romeo', "issn='$issn'");
-    my $rec;
-    if ($rec = $self->{'db'}->next ($rs)) {
-        $self->{'cache'}{$issn} = [$rec->{'color'}, $rec->{'id'}];
-        return ($rec->{'color'}, $rec->{'id'});
+        return ($self->{'cache'}{$issn});
     } else {
-        $self->{'cache'}{$issn} = ['none', 0];
-        return ('none', 0);
+        return (['none', '']);
     }
-#   FIX - add clearing of cache if needed or pre-load the cache based on performance
 }
 
 1;
