57e7dfb34da7a8f951d5b28ff935add8bb7e742a
franck <franck@cvt.dk>
2015-11-19 13:38:54 +0100

    Added more fields to the BFI table, a cleanup of research_area and new methods to access BFI level and research_area.

--- lib/OA/Indicator/DB/BFI.pm
+++ lib/OA/Indicator/DB/BFI.pm
@@ -22,8 +22,11 @@ sub create
     my $db = $self->{'db'};
     $db->sql ('create table if not exists bfi (
                    id              text primary key,
-                   publication_id  text,
+                   harvest_stamp   integer,
+                   harvest_date    text,
                    source          text,
+                   source_id       text,
+                   publication_id  text,
                    doc_type        text,
                    doc_review      text,
                    doc_level       text,
@@ -37,10 +40,10 @@ sub create
                    lang            text,
                    issn            text,
                    eissn           text,
-                   authors         text,
                    mods            text,
                    original_xml    text
               )');
+    $db->sql ('create index if not exists bfi_source_id on bfi (source_id)');
     $db->sql ('create index if not exists bfi_publication_id on bfi (publication_id)');
 }
 
@@ -62,10 +65,27 @@ sub load
         }
         my $rec = {};
         my @fields = split ("\t");
-        foreach my $fld (qw(id publication_id source doc_type doc_review doc_level class research_area journal_level fraction point cooperation title lang issn eissn authors)) {
+        foreach my $fld (qw(id harvest_stamp harvest_date status source source_id publication_id doc_type doc_review doc_level class research_area journal_level fraction point cooperation title lang issn eissn)) {
             $rec->{$fld} = shift (@fields);
         }
+        $rec->{'id'} = $rec->{'source_id'};
+        if ($rec->{'status'} eq 'deleted') {
+            next;
+        } else {
+            delete ($rec->{'status'});
+        }
         $rec->{'source'} = lc ($rec->{'source'});
+        if ($rec->{'research_area'} eq 'HUM') {
+            $rec->{'research_area'} = 'hum';
+        } elsif ($rec->{'research_area'} eq 'SAM') {
+            $rec->{'research_area'} = 'soc';
+        } elsif ($rec->{'research_area'} eq 'SUND') {
+            $rec->{'research_area'} = 'med';
+        } elsif ($rec->{'research_area'} eq 'NAT/TEK') {
+            $rec->{'research_area'} = 'sci';
+        } else {
+            $self->{'oai'}->log ('e',  'unknown research area: ' . $rec->{'research_area'});
+        }
         if (!$rec->{'journal_level'}) {
             $rec->{'journal_level'} = 0;
         } else {
@@ -87,12 +107,38 @@ sub pubid
 {
     my ($self, $id) = @_;
 
-    my $rs = $self->{'db'}->select ('publication_id', 'bfi', "id='$id'");
+    my $rs = $self->{'db'}->select ('publication_id', 'bfi', "source_id='$id'");
     my $rec;
     if ($rec = $self->{'db'}->next ($rs)) {
         return ($rec->{'publication_id'});
     } else {
         return (-1);
+    }
+}
+
+sub level
+{
+    my ($self, $id) = @_;
+
+    my $rs = $self->{'db'}->select ('journal_level', 'bfi', "source_id='$id'");
+    my $rec;
+    if ($rec = $self->{'db'}->next ($rs)) {
+        return ($rec->{'journal_level'});
+    } else {
+        return (0);
+    }
+}
+
+sub research_area
+{
+    my ($self, $id) = @_;
+
+    my $rs = $self->{'db'}->select ('research_area', 'bfi', "source_id='$id'");
+    my $rec;
+    if ($rec = $self->{'db'}->next ($rs)) {
+        return ($rec->{'research_area'});
+    } else {
+        return ('');
     }
 }
 
