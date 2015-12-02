e4d7be7e85a20a0323c0fa3c2a466b706a91c8c8
franck <franck@cvt.dk>
2015-12-02 23:04:23 +0100

    Added a number of fulltext related fields and updating them which checking on potential fulltext request.

--- lib/OA/Indicator/Segments/Fetch.pm
+++ lib/OA/Indicator/Segments/Fetch.pm
@@ -25,7 +25,14 @@ sub process
     my $rec;
     while ($rec = $self->{'db'}->next ($rs)) {
         $count->{'total'}++;
-        $records->{$rec->{'id'}} = {id => $rec->{'id'}};
+        $records->{$rec->{'id'}} = {
+            id                  => $rec->{'id'},
+            fulltext_link       => 0,
+            fulltext_link_oa    => 0,
+            fulltext_downloaded => 0,
+            fulltext_verified   => 0,
+            fulltext_pdf        => 0,
+        };
     }
     $self->{'oai'}->log ('i', "starting fulltext checks of $count->{'total'} records");
     my $mxd = new OA::Indicator::DB::MXD ($self->{'db'}, $self->{'oai'});
@@ -33,19 +40,25 @@ sub process
     $ft->create ();
     foreach my $id (keys (%{$records})) {
         if (!$mxd->fulltext_exists ($id)) {
+            $self->update ($records->{$id});
             $count->{'noft'}++;
             next;
         }
         if ($mxd->fulltext_type ($id) ne 'digital_object') {
+            $self->update ($records->{$id});
             $count->{'notdo'}++;
             next;
         }
+        $records->{$id}{'fulltext_link'} = 1;
         if ($mxd->fulltext_access ($id) ne 'oa') {
+            $self->update ($records->{$id});
             $count->{'notoa'}++;
             next;
         }
+        $records->{$id}{'fulltext_link_oa'} = 1;
         $count->{'requests'}++;
         $ft->request ($id, $mxd->fulltext_uri ($id), $mxd->fulltext_size ($id), $mxd->fulltext_mime ($id), $mxd->fulltext_filename ($id));
+        $self->update ($records->{$id});
     }
     $self->{'oai'}->log ('i', "placed $count->{'requests'} requests out of $count->{'total'} records");
     $self->{'oai'}->log ('i', "$count->{'noft'} have no fulltext");
@@ -54,6 +67,14 @@ sub process
     $ft->harvest ();
     $self->{'oai'}->log ('i', 'done');
     return (1);
+}
+
+sub update
+{
+    my ($self, $rec) = @_;
+
+
+    $self->{'db'}->update ('records', 'id', $rec);
 }
 
 1;
