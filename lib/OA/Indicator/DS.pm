f4112f1414e966ecc6c9bfb1b16a7cfa0437f5af
franck <franck@cvt.dk>
2019-06-06 13:06:45 +0200

    Updated parameters to reflect DS2 changes

--- lib/OA/Indicator/DS.pm
+++ lib/OA/Indicator/DS.pm
@@ -11,7 +11,7 @@ sub new
 {
     my ($class) = @_;
 
-    my $self = {channel_id => 1, exchange_name => 'cvt.direct', request_id => 'oaindicator', queue_name => 'oa.export',
+    my $self = {channel_id => 3, exchange_name => 'cvt.direct', bind_exchange_name => 'cvt.topic', request_id => 'oaindicator', queue_name => 'oa.export',
                 queue_opt => {auto_delete => 1, durable => 1}, queue_arg => {'x-max-priority' => 10}, error => {code => 0}};
 
     return (bless ($self, $class));
@@ -33,7 +33,7 @@ sub init
     $mq->basic_qos ($self->{'channel_id'}, {prefetch_count => 20});
     $self->{'queue'} = $mq->queue_declare($self->{'channel_id'}, $self->{'queue_name'}, $self->{'queue_opt'}, $self->{'queue_arg'});
     $mq->consume ($self->{'channel_id'}, $self->{'queue_name'}, {no_ack => 1});
-    $mq->queue_bind ($self->{'channel_id'}, $self->{'queue_name'}, $self->{'exchange_name'}, 'export.' . $self->{'request_id'} . '.#');
+    $mq->queue_bind ($self->{'channel_id'}, $self->{'queue_name'}, $self->{'bind_exchange_name'}, 'export.' . $self->{'request_id'} . '.#');
     $self->{'mq'} = $mq;
 }
 
